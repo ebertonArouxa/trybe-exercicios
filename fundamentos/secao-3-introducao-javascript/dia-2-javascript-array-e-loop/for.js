72da93f44e317607af9fbb8c7441c01cdc2c9283
Eberton <ebertonsouza@unigranrio.br>
2023-03-29 15:49:11 -0300

    acrescenta conteudo ao for

--- fundamentos/secao-3-introducao-javascript/dia-2-javascript-array-e-loop/for.js
+++ fundamentos/secao-3-introducao-javascript/dia-2-javascript-array-e-loop/for.js
@@ -3,3 +3,9 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let list = 0; list < groceryList.length; list += 1) {
     console.log(groceryList[list]);
 }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let index of names) {
+    console.log(index);
+}
