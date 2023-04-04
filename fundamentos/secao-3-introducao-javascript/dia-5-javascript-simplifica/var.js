657426a19243d49cbb3ec1add926a9f4b27402d2
Eberton <ebertonsouza@unigranrio.br>
2023-04-04 19:19:45 -0300

    corrige exercicio 2

--- fundamentos/secao-3-introducao-javascript/dia-5-javascript-simplifica/var.js
+++ fundamentos/secao-3-introducao-javascript/dia-5-javascript-simplifica/var.js
@@ -4,3 +4,14 @@ function imprimeIdade() {
     }
   }
   imprimeIdade()
+
+    // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+    const pessoa = {
+        nome: 'Henri',
+        idade: 20
+      }
+      pessoa.nome = 'Luna';
+      pessoa.idade = 19;
+        
+      console.log('Nome:', pessoa.nome);
+      console.log('Idade:', pessoa.idade);
